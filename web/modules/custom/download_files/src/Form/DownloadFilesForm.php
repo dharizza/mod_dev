93e33f2981f0dd8842d0e744f74bb27a146c1354
Dharizza Espinach B <dharizza@gmail.com>
2021-03-18 09:26:57 -0600

    Add download files form.

---
+++ web/modules/custom/download_files/src/Form/DownloadFilesForm.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\download_files\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a Download files form.
+ */
+class DownloadFilesForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'download_files';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['file'] = [
+      '#type' => 'select',
+      '#title' => $this->t('File name'),
+      '#description' => $this->t('Select the file that you want to download.'),
+      '#required' => TRUE,
+      '#options' => $this->getFiles(),
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Download'),
+    ];
+
+    return $form;
+  }
+
+  public function getFiles() {
+    $query = \Drupal::database()->select('file_managed', 'f')
+      ->fields('f', ['filename', 'uri'])
+      ->execute()
+      ->fetchAll();
+    $files = [];
+    foreach ($query as $file) {
+      $files[$file->uri] = $file->filename;
+    }
+    return $files;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (mb_strlen($form_state->getValue('message')) < 10) {
+      $form_state->setErrorByName('name', $this->t('Message should be at least 10 characters.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->messenger()->addStatus($this->t('The message has been sent.'));
+    $form_state->setRedirect('<front>');
+  }
+
+}
